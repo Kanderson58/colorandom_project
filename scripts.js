8aea744383c0c6cb781cef11b7a9f5d409672a78
Brianne Bourassa <shinybri1011@gmail.com>
2023-01-02 16:15:40 -0700

    add planning and pseudocode for iteration 1

--- scripts.js
+++ scripts.js
@@ -13,30 +13,33 @@ class Palette{
 };
 
 
-
-
 var array = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
-
 
 window.addEventListener('load', randomIndex)
 
 function randomIndex(array){
     return array[Math.floor(Math.random()*array.length)]
-    
 };
-// console.log(randomIndex(array))
-// console.log(randomIndex(array))
-// console.log(array[0]+array[13])
 
-function getRandomPalette(){
+function getRandomHexCode(){
     var hexCode = '#'
-    for(var i = 0; i < 6; i++ ){
+    for(var i = 0; i < 6; i++){
         hexCode += randomIndex(array)
     }
     return hexCode
+    // put into new instance of Color
+
+    // push into colors array in Palette Class
+    // new instance of Palette
+};
+console.log(getRandomHexCode());
+
+function getRandomPalette(){
+    // for loop
+    // get hex code x5
+    //push into array, make new instance 
 };
 
-console.log(getRandomPalette())
 
 
 function lockColors(){
