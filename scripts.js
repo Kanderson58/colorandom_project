8efb2fa320fc0b8e730bf9a3f03ba93cb8f13f0c
Isaiah Richardson <capcinematic@gmail.com>
2023-01-03 16:12:03 -0700

    Develop  palette functionality

--- scripts.js
+++ scripts.js
@@ -11,12 +11,25 @@ class Palette{
         this.colors = [];
 
     }
+    lockColor(event){
+        for(var i = 0; i < this.colors.length; i++){
+            if (event.target.class === this.colors[i].hexCode){
+                this.colors[i].locked = true;
+                console.log(this.colors[i].locked)
+            }
+        }
+        
+    }
 };
 
 
 var array = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
+var lockedColors = []
+var currentPalette = document.querySelector(".current-palette")
+var randomPalette;
 
 window.addEventListener('load', getRandomPalette)
+currentPalette.addEventListener('click', randomPalette.lockColor)
 
 function randomIndex(array){
     return array[Math.floor(Math.random()*array.length)]
@@ -28,24 +41,26 @@ function getRandomHexCode(){
         hexCode += randomIndex(array)
     }
     return hexCode
-    // put into new instance of Color
-
-    // push into colors array in Palette Class
-    // new instance of Palette
-};
-// console.log(getRandomHexCode());
-
-function getRandomPalette(){
-    var randomPalette = new Palette
-    for(var i = 0; i < 5; i++){
-       randomPalette.colors.push(new Color(getRandomHexCode()))
-       console.log(randomPalette.colors[0].hexCode) 
-    }
-
-};
-
-
-
-function lockColors(){
     
 };
+
+
+function getRandomPalette(){
+    randomPalette = new Palette
+    for(var i = 1; i < 6; i++){
+        var thisHex = getRandomHexCode()
+       randomPalette.colors.push(new Color(thisHex))
+       currentPalette.innerHTML +=`
+    <fieldset class="${thisHex}">
+       <div class="boxes color${i}">test text</div>
+       <label>${thisHex}</label>
+   </fieldset>`
+     }
+
+};
+// targetting a unique id
+
+
+// function lockColor(){
+    
+// };
