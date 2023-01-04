61c38f89b49c10f5ed7f0bc2cd138f8106652e19
Shane Misra <shane.misra@gmail.com>
2023-01-03 17:05:21 -0700

    Enable locking mechanism for our color classes

--- scripts.js
+++ scripts.js
@@ -11,17 +11,18 @@ class Palette{
         this.colors = [];
 
     }
-    lockColor(event){
-        for(var i = 0; i < this.colors.length; i++){
-            if (event.target.class === this.colors[i].hexCode){
-                this.colors[i].locked = true;
-                console.log(this.colors[i].locked)
-            }
-        }
+    // lockColor(){
+    //     console.log('hello');
+    //     for(var i = 0; i < this.colors.length; i++){
+    //         if (event.target.class === this.colors[i].hexCode){
+    //             this.colors[i].locked = true;
+    //             console.log(this.colors[i].locked)
+    //         }
+    //     }
         
-    }
+    // }
 };
-
+// How might we use our lockColor function within the palette class? 
 
 var array = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
 var lockedColors = []
@@ -29,7 +30,7 @@ var currentPalette = document.querySelector(".current-palette")
 var randomPalette;
 
 window.addEventListener('load', getRandomPalette)
-currentPalette.addEventListener('click', randomPalette.lockColor)
+currentPalette.addEventListener('click', lockColor)
 
 function randomIndex(array){
     return array[Math.floor(Math.random()*array.length)]
@@ -44,23 +45,35 @@ function getRandomHexCode(){
     
 };
 
-
 function getRandomPalette(){
     randomPalette = new Palette
     for(var i = 1; i < 6; i++){
         var thisHex = getRandomHexCode()
        randomPalette.colors.push(new Color(thisHex))
-       currentPalette.innerHTML +=`
-    <fieldset class="${thisHex}">
+       currentPalette.innerHTML +=
+       `
+       <fieldset class="${thisHex}">
        <div class="boxes color${i}">test text</div>
        <label>${thisHex}</label>
-   </fieldset>`
+       </fieldset>
+       `;
+
+
      }
 
 };
-// targetting a unique id
-
-
-// function lockColor(){
+ // Great place to practice refactoring:
+function lockColor(event) {
+    console.log(event.target.parentNode.classList.value);
+    for(var i = 0; i < randomPalette.colors.length; i++) {
+        if (event.target.parentNode.classList.value === randomPalette.colors[i].hexCode && !randomPalette.colors[i].locked) {
+            randomPalette.colors[i].locked = true;
+            console.log(randomPalette.colors[i].locked);
+        }
+        else if  (event.target.parentNode.classList.value === randomPalette.colors[i].hexCode){
+            randomPalette.colors[i].locked = false;
+            console.log(randomPalette.colors[i].locked);
+        }
+    }
     
-// };
+}
