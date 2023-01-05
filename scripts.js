38ae8284c6b63bf5ea1f083562c09c9f18cdb850
Kara Anderson <kanderson8585@gmail.com>
2023-01-05 14:42:13 -0700

    Refactor DOM vs data model updates

--- scripts.js
+++ scripts.js
@@ -19,26 +19,11 @@ class Palette{
         this.lockedColors = [];
     }
     getRandomPalette(){
-        var currentPalette = document.querySelector(".current-palette")
-        currentPalette.innerHTML = ''
         this.colors = [];
-        var randomColor;
-        this.displayLockedColors()
-        // console.log(this.lockedColors)
         for(var i = this.lockedColors.length; i < 5; i++){
-            randomColor = new Color
+            var randomColor = new Color
             randomColor.randomIndex()
             this.colors.push(randomColor)
-            currentPalette.innerHTML +=
-            `
-            <fieldset class="${randomColor.hexCode}">
-            <div class="boxes color${i}"></div>
-            <img src="./assets/unlock.png">
-            <label>${randomColor.hexCode}</label>
-            </fieldset>
-            `;
-            var colorBrick = document.querySelector(`.color${i}`)
-            colorBrick.style.backgroundColor = `${randomColor.hexCode}`
          }
     };
     lockThisColor(currentColorHex) {
@@ -47,9 +32,6 @@ class Palette{
                 this.colors[i].locked = true;
                 this.lockedColors.push(this.colors[i])
                 console.log(this.lockedColors)
-                // this.toggleLock()
-                // this.displayLockedColors()
-                // this.checkForDoubles(currentColorHex)
             } else if (currentColorHex === this.colors[i].hexCode && this.lockedColors.length <= 5){
                 this.colors[i].locked = false;
                 this.lockedColors.splice(this.lockedColors.indexOf(this.colors[i]), 1);
@@ -57,72 +39,53 @@ class Palette{
             } 
         } 
     }
-
-    displayLockedColors(){
-        for(var i = 0; i < this.lockedColors.length; i++){
-            currentPalette.innerHTML +=
-            `
-            <fieldset class="${this.lockedColors[i].hexCode}">
-            <div class="boxes color${i}"></div>
-            <img src="./assets/lock.png">
-            
-            <label>${this.lockedColors[i].hexCode}</label>
-            
-            </fieldset>
-            `;
-            var colorBrick = document.querySelector(`.color${i}`)
-            colorBrick.style.backgroundColor = `${this.lockedColors[i].hexCode}`
-         }
-    };
-
-    // toggleLock(click){
-    //     // console.log(colorObject)
-    //     var lock = document.querySelector('img')
-    //     if(colorObject.locked){
-    //         lock.classList.remove('.hidden')
-    //     } else {
-    //         lock.classList.add('.hidden')
-    //     }
-    //     click.classList.remove('hidden')
+    // displayLockedColors(){
+    //     for(var i = 0; i < this.lockedColors.length; i++){
+    //         currentPalette.innerHTML +=
+    //         `
+    //         <fieldset class="${this.lockedColors[i].hexCode}">
+    //         <div class="boxes color${i}"></div>
+    //         <label>${this.lockedColors[i].hexCode}</label>
+    //         </fieldset>
+    //         `;
+    //         var colorBrick = document.querySelector(`.color${i}`)
+    //         colorBrick.style.backgroundColor = `${this.lockedColors[i].hexCode}`
+    //      }
     // };
-
-//     checkForDoubles(givenColorHex) {
-//         var doubles = [];
-//         for(var i = 0; i < this.lockedColors.length; i++) {
-//             if(givenColorHex === this.lockedColors[i].hexCode){
-//                 doubles.push(this.lockedColors[i].hexCode)
-//             }
-//             if(doubles.length >= 2) {
-//                 this.lockedColors.splice(i, 1)
-//             }
-//         }
-//     }
 };
 
-var randomPalette = new Palette
+var currentPalette = new Palette
 var newPaletteButton = document.querySelector(".new-button")
-var currentPalette = document.querySelector(".current-palette")
+var currentPaletteSection = document.querySelector(".current-palette")
 
-window.addEventListener('load', function() {randomPalette.getRandomPalette()})
-newPaletteButton.addEventListener('click', getNewRandom)
-currentPalette.addEventListener('click', lockColor)
+window.addEventListener('load', displayCurrent)
+// newPaletteButton.addEventListener('click', getNewRandom)
+currentPaletteSection.addEventListener('click', lockColor)
 
 function getNewRandom() {
-    if(randomPalette.lockedColors.length < 5) {
-        randomPalette.getRandomPalette()
+    if(currentPalette.lockedColors.length < 5) {
+        currentPalette.getRandomPalette()
     } else {
         alert("Uh oh!  All the colors are locked!")
     }
 }
 
 function lockColor(event) {
-    randomPalette.lockThisColor(event.target.parentNode.classList.value);
-    // var lock = event.target.nextElementSibling
-    // randomPalette.toggleLock(lock)
-    // if(colorObject.locked){
-    //     lock.classList.remove('.hidden')
-    // } else {
-    //     lock.classList.add('.hidden')
-    // }
-    // console.log(event.target.nextElementSibling)
+    currentPalette.lockThisColor(event.target.parentNode.classList.value);
+}
+
+function displayCurrent() {
+    currentPalette.getRandomPalette()
+    currentPaletteSection.innerHTML = ''
+    for(var i = 0; i < 5; i++){
+        currentPaletteSection.innerHTML +=
+        `
+        <fieldset class="${currentPalette.colors[i].hexCode}">
+        <div class="boxes color${i}"></div>
+        <label>${currentPalette.colors[i].hexCode}</label>
+        </fieldset>
+        `;
+        var colorBrick = document.querySelector(`.color${i}`)
+        colorBrick.style.backgroundColor = `${currentPalette.colors[i].hexCode}`
+    }
 }
