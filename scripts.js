191ad32d2446c22ef4e73ee3037c2b9c1dd980a4
Kara Anderson <kanderson8585@gmail.com>
2023-01-07 09:23:01 -0700

    CSS cleanup and prevent doubles on saved

--- scripts.js
+++ scripts.js
@@ -6,9 +6,9 @@ class Color{
     randomIndex(){
         this.hexCode ='#'
         for(var i = 0; i < 6; i++){
-            var possibleHexes = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
-            this.hexCode += possibleHexes[Math.floor(Math.random()*possibleHexes.length)]
-        } return this.hexCode
+            var possibleHexes = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
+            this.hexCode += possibleHexes[Math.floor(Math.random()*possibleHexes.length)];
+        } return this.hexCode;
     };
 };
 
@@ -17,78 +17,74 @@ class Palette{
         this.id = Date.now();
         this.colors = [];
         this.lockedColors = [];
+        this.savedColors = [];
     }
     getRandomPalette(){
+        this.id = Date.now();
         this.colors = [];
         if(this.lockedColors.length !== 0) {
             for(var i = 0; i < this.lockedColors.length; i++) {
                 this.colors.push(this.lockedColors[i]);
-                console.log(this.colors)
             }
         }
         for(var i = this.lockedColors.length; i < 5; i++){
-            var randomColor = new Color
-            randomColor.randomIndex()
-            this.colors.push(randomColor)
+            var randomColor = new Color;
+            randomColor.randomIndex();
+            this.colors.push(randomColor);
          }
     };
     lockThisColor(currentColorHex) {
         for(var i = 0; i < this.colors.length; i++) {
             if (currentColorHex === this.colors[i].hexCode && !this.colors[i].locked && this.lockedColors.length < 5) {
-                this.colors[i].locked = true;
-                this.lockedColors.push(this.colors[i])
-                console.log(this.lockedColors)
+                this.addToLocked(this.colors[i]);
             } else if (currentColorHex === this.colors[i].hexCode && this.lockedColors.length <= 5){
-                this.colors[i].locked = false;
-                this.lockedColors.splice(this.lockedColors.indexOf(this.colors[i]), 1);
+                this.removeFromLocked(this.colors[i]);
             } 
         } 
     }
+    addToLocked(color) {
+        color.locked = true;
+        this.lockedColors.push(color);
+    }
+    removeFromLocked(color) {
+        color.locked = false;
+        this.lockedColors.splice(this.lockedColors.indexOf(color), 1);
+    }
 };
 
-var currentPalette = new Palette
-var newPaletteButton = document.querySelector(".new-button")
-var currentPaletteSection = document.querySelector(".current-palette")
-var savedSection = document.querySelector(".saved-palettes")
-var savedButton = document.querySelector(".save-button")
+var currentPalette = new Palette;
+var savedPalettes = [];
+var newPaletteButton = document.querySelector(".new-button");
+var currentPaletteSection = document.querySelector(".current-palette");
+var savedSection = document.querySelector(".saved-palettes");
+var savedButton = document.querySelector(".save-button");
 
 
-window.addEventListener('load', displayCurrent)
-newPaletteButton.addEventListener('click', displayCurrent)
-currentPaletteSection.addEventListener('click', lockColor)
-savedButton.addEventListener('click', displaySavedPalette)
+window.addEventListener('load', displayCurrent);
+newPaletteButton.addEventListener('click', displayCurrent);
+currentPaletteSection.addEventListener('click', lockColor);
+savedButton.addEventListener('click', displaySavedPalette);
 
 function lockColor(event) {
     currentPalette.lockThisColor(event.target.parentNode.classList.value);
-    // toggleLockIcon(event)
-    event.target.parentNode.children[1].children[0].src="./lock.png"
-    console.log(event.target.parentNode.previousSibling.nextSibling.children[1].children[0].src)
+    toggleToLock(event);
 };
 
-function toggleLockIcon(event){
-    
-    // for(i = 0; i < currentPalette.colors.length; i++){
-    //     var lockEmojiToggle = document.querySelectorAll(".toggle-lock")
-    //     if (currentPalette.colors[i].locked){
-    //         lockEmojiToggle.innerHTML = '<img src= "./lock.png"/>'
-    //         console.log(currentPalette.colors[i].locked)
-    //     } else {
-    //         lockEmojiToggle.innerHTML = '<img src= "./unlock.png"/>'
-    //         console.log(currentPalette.colors[i].locked)
-    //     }
-    // }
-    // if(colors.locked === false){
-
-    // }
+function toggleToLock(event){
+    for(var i = 0; i < currentPalette.colors.length; i++) {
+        if(currentPalette.colors[i].locked === true) {
+            event.target.parentNode.children[1].children[0].src = "./lock.png";
+        } 
+        // else {
+        //     event.target.parentNode.children[1].children[0].src = "./unlock.png";
+        // }
+    }
 };
 
 function displayCurrent() {
-    currentPalette.getRandomPalette()
+    currentPalette.getRandomPalette();
     currentPaletteSection.innerHTML = ''
     for(var i = 0; i < 5; i++){
-        // if(currentPalette.colors[i].locked){
-
-        
         currentPaletteSection.innerHTML +=
         `
         <fieldset class="${currentPalette.colors[i].hexCode}">
@@ -96,26 +92,16 @@ function displayCurrent() {
         <label><img src= "./unlock.png"/>${currentPalette.colors[i].hexCode}</label>
         </fieldset>
         `;
-        var colorBrick = document.querySelector(`.color${i}`)
-        colorBrick.style.backgroundColor = `${currentPalette.colors[i].hexCode}`
-        // } else {
-        //     currentPaletteSection.innerHTML +=
-        // `
-        // <fieldset class="${currentPalette.colors[i].hexCode}">
-        // <div class="boxes color${i}"></div>
-        // <label><img src="./unlock.png">${currentPalette.colors[i].hexCode}</label>
-        // </fieldset>
-        // `;
-        // var colorBrick = document.querySelector(`.color${i}`)
-        // colorBrick.style.backgroundColor = `${currentPalette.colors[i].hexCode}`
-        // }
+        var colorBrick = document.querySelector(`.color${i}`);
+        colorBrick.style.backgroundColor = `${currentPalette.colors[i].hexCode}`;
     }
-    toggleLockIcon()
 };
-// Instead of if else HTML just If Else IMG
 
 function displaySavedPalette() {
-    savedSection.innerHTML += `<div class="lil-box-container" id="${currentPalette.id}"></div>`
+    if(savedPalettes.includes(currentPalette.id)) {
+        return
+    }
+    savedSection.innerHTML += `<div class="lil-box-container" id="${currentPalette.id}"></div>`;
     var lilBoxContainer = savedSection.lastChild
     for(var i = 0; i < 5; i++){
         lilBoxContainer.innerHTML +=
@@ -126,5 +112,6 @@ function displaySavedPalette() {
         </fieldset>
         `;
         }
-        lilBoxContainer.innerHTML += '<button class="trash">🗑️</button>'
+        lilBoxContainer.innerHTML += '<button class="trash">🗑️</button>';
+        savedPalettes.push(currentPalette.id);
 };
